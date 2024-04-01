24e40e7a80939d3dd3ee5f78ca59ce160d86922a
Juan Jose Melero <meleros_paw@hotmail.com>
2024-04-01 21:39:38 +0200

    Añadido header-behaviour como módulo.

--- src/main/java/com/melerospaw/coordinatorbehaviour/HeaderBehaviour.java
+++ src/main/java/com/melerospaw/coordinatorbehaviour/HeaderBehaviour.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import java.util.LinkedList;
 import java.util.List;
 
